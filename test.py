9a57ac115a702eaed4957e6600a405f24d41eb2c
TrentBrunson <bbiz2@icloud.com>
2021-11-14 14:11:18 -0600

    cleaned and transformed data

--- test.py
+++ test.py
@@ -6,6 +6,26 @@ file = 'data\heart.csv'
 df = pd.read_csv(file)
 df
 # %%
+(df == 0).sum(axis=1)
+#%%
+df.isin([0]).sum(axis=1)
+#%%
+df.describe()
+#%%
+cleanedDF = df.RestingBP.mask(df.RestingBP == 0, 132)
+
+# df.loc[(df.RestingBP == 0), 'RestingBP']=132
+cleanedDF
+#%%
+cleanedDF.describe()
+#%%
+df
+#%%
+df.RestingBP.mask(df.RestingBP == 0, 132, inplace=True)
+df
+#%%
+print(df.describe())
+# %%
 # This library captures most of the EDA done above
 # https://pandas-profiling.github.io/pandas-profiling/docs/master/index.html
 profile = ProfileReport(df, title="Cardiovascular Disease Data Profiling Report", explorative=True)
@@ -13,4 +33,5 @@ profile = ProfileReport(df, title="Cardiovascular Disease Data Profiling Report"
 profile.to_notebook_iframe()
 # %%
 profile.to_file("index.html")
+
 # %%
