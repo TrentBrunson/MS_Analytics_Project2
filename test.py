5bc129e52555db14f1c81734a9734bd9855081b7
TrentBrunson <bbiz2@icloud.com>
2021-11-14 18:56:31 -0600

    last to rename

--- test.py
+++ test.py
@@ -1,10 +1,34 @@
 #%%
 import pandas as pd
-from pandas_profiling import ProfileReport
+# from pandas_profiling import ProfileReport
+import matplotlib.pyplot as plt
+import seaborn as sns
 #%%
 file = 'data\heart.csv'
 df = pd.read_csv(file)
 df
+#%%
+fig, axs = plt.subplots(len(df.columns), figsize=(5, 20))
+for n, col in enumerate(df.columns):
+    # print(col)
+    a = df[col].hist(ax=axs[n])
+    a.set_title(col)
+
+fig.tight_layout()
+#%%
+plt.figure(figsize=(8,8))
+sns.displot(df['Age'], color="red", label="Age", kde= True)
+plt.legend()
+plt.savefig("images\CVD_by_Age.png")
+#%%
+plt.figure(figsize=(8,8))
+sns.displot(df['Cholesterol'], color="blue", label="Cholesterol")
+plt.legend()
+plt.savefig("images\C2.png")
+#%%
+df.Cholesterol.plot(kind = "hist", bins = 20, figsize = (8,5))
+plt.legend()
+plt.savefig("images\cholesterol.png")
 # %%
 (df == 0).sum(axis=1)
 #%%
